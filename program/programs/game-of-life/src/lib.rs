82eea2672f7547e83aa6d32769e6953285fb6c14
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-18 00:29:30 +0200

    board

--- program/programs/game-of-life/src/lib.rs
+++ program/programs/game-of-life/src/lib.rs
@@ -1,9 +1,5 @@
 use anchor_lang::prelude::*;
 
-use shared::PDA_SEED_GRID;
-
-pub mod shared;
-
 declare_id!("BwocGJZSrrxfE3dSWVfCiL7AQSKRCjYbeHs4vErYEfmN");
 
 #[program]
@@ -11,23 +7,28 @@ pub mod game_of_life {
 
     use super::*;
 
-    pub fn initialize_board(ctx: Context<InitializeBoard>, packed_grid: [u32; 32]) -> Result<()> {
-        ctx.accounts.grid.packed_grid = packed_grid;
+    pub fn initialize_board(
+        ctx: Context<InitializeBoard>,
+        _nft_pubkey: Pubkey,
+        packed_board: [u32; 32],
+    ) -> Result<()> {
+        ctx.accounts.board.packed_board = packed_board;
 
         Ok(())
     }
 }
 
 #[derive(Accounts)]
+#[instruction(nft_pubkey: Pubkey)]
 pub struct InitializeBoard<'info> {
     #[account(
-        init_if_needed,
+        init,
         payer = signer,
         space = 128 + 8,
-        seeds=[PDA_SEED_GRID.as_ref()],
+        seeds=[nft_pubkey.as_ref()],
         bump
     )]
-    pub grid: Account<'info, Grid>,
+    pub board: Account<'info, Board>,
 
     #[account(mut)]
     pub signer: Signer<'info>,
@@ -36,6 +37,6 @@ pub struct InitializeBoard<'info> {
 }
 
 #[account]
-pub struct Grid {
-    packed_grid: [u32; 32],
+pub struct Board {
+    packed_board: [u32; 32],
 }
--- program/programs/game-of-life/src/shared/constants.rs
+++
@@ -1,2 +0,0 @@
-pub const GRID_SIZE: usize = 32;
-pub const PDA_SEED_GRID: &[u8; 4] = b"grid";

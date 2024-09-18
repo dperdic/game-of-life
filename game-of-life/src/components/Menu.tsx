099f1165bd6da3a5c15b4261f574351253491b11
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-18 13:07:20 +0200

    menu

---
+++ game-of-life/src/components/Menu.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Menu() {
+  return (
+    <div className="flex flex-col justify-center gap-4 overflow-hidden overflow-x-auto rounded-lg border bg-white p-4 shadow">
+      <h3 className="text-center font-semibold">Menu</h3>
+    </div>
+  );
+}

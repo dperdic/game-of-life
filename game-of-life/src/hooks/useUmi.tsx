fc2febdec1e158d68b4c1c83e11bfe561883bb98
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-18 21:22:53 +0200

    dasApiRpc

--- game-of-life/src/hooks/useUmi.tsx
+++ game-of-life/src/hooks/useUmi.tsx
@@ -3,6 +3,7 @@ import { UmiContext } from "@/providers/UmiContext";
 
 export default function useUmi() {
   const umi = useContext(UmiContext).umi;
+  const dasApiRpc = useContext(UmiContext).dasApiRpc;
 
-  return { umi };
+  return { umi, dasApiRpc };
 }

050a7a721279d979d28aebd9d862b6afbd18eea7
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-18 21:22:40 +0200

    dasApi support

--- game-of-life/src/providers/UmiContext.ts
+++ game-of-life/src/providers/UmiContext.ts
@@ -1,12 +1,15 @@
-import { Umi } from "@metaplex-foundation/umi";
+import { DasApiInterface } from "@metaplex-foundation/digital-asset-standard-api";
+import { RpcInterface, Umi } from "@metaplex-foundation/umi";
 import { createContext } from "react";
 
 export type UmiContext = {
   umi: Umi | null;
+  dasApiRpc: (RpcInterface & DasApiInterface) | null;
 };
 
 const DEFAULT_CONTEXT: UmiContext = {
   umi: null,
+  dasApiRpc: null,
 };
 
 export const UmiContext = createContext<UmiContext>(DEFAULT_CONTEXT);
--- game-of-life/src/providers/UmiContextProvider.tsx
+++ game-of-life/src/providers/UmiContextProvider.tsx
@@ -1,11 +1,16 @@
 import { ReactNode, useEffect, useState } from "react";
-import type { Umi } from "@metaplex-foundation/umi";
+import type { RpcInterface, Umi } from "@metaplex-foundation/umi";
 import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { mplBubblegum } from "@metaplex-foundation/mpl-bubblegum";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { UmiContext } from "@/providers/UmiContext";
+import {
+  createDasApiDecorator,
+  dasApi,
+  DasApiInterface,
+} from "@metaplex-foundation/digital-asset-standard-api";
 
 export default function UmiContextProvider({
   children,
@@ -15,19 +20,31 @@ export default function UmiContextProvider({
   const wallet = useWallet();
   const { connection } = useConnection();
   const [umi, setUmi] = useState<Umi | null>(null);
+  const [dasApiRpc, setDasApiRpc] = useState<
+    (RpcInterface & DasApiInterface) | null
+  >(null);
 
   useEffect(() => {
     let umi: Umi | null = null;
+    let dasApiRpc: (RpcInterface & DasApiInterface) | null = null;
 
     if (wallet.publicKey) {
       umi = createUmi(connection.rpcEndpoint, { commitment: "confirmed" })
         .use(walletAdapterIdentity(wallet))
         .use(mplTokenMetadata())
-        .use(mplBubblegum());
+        .use(mplBubblegum())
+        .use(dasApi());
+
+      dasApiRpc = createDasApiDecorator(umi.rpc);
     }
 
     setUmi(umi);
+    setDasApiRpc(dasApiRpc);
   }, [connection.rpcEndpoint, wallet]);
 
-  return <UmiContext.Provider value={{ umi }}>{children}</UmiContext.Provider>;
+  return (
+    <UmiContext.Provider value={{ umi, dasApiRpc }}>
+      {children}
+    </UmiContext.Provider>
+  );
 }

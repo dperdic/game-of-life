44c36d93d58ffbd9a03ddcc5a8b9cf6028d4c319
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-18 21:23:27 +0200

    menu wip

--- game-of-life/src/components/Menu.tsx
+++ game-of-life/src/components/Menu.tsx
@@ -1,9 +1,57 @@
 "use client";
 
+import useMinter from "@/hooks/useMinter";
+import useUmi from "@/hooks/useUmi";
+import { publicKey } from "@metaplex-foundation/umi";
+import { useEffect } from "react";
+
 export default function Menu() {
+  const { umi, dasApiRpc } = useUmi();
+  const { mintToCollection } = useMinter();
+
+  const handleStartNewGame = async () => {
+    const cNftId = await mintToCollection("Game of life test", "$GOL", "");
+
+    console.log(cNftId!.toString());
+  };
+
+  useEffect(() => {
+    if (!umi || !dasApiRpc) {
+      return;
+    }
+
+    dasApiRpc
+      .getAssetsByOwner({
+        owner: publicKey("3G4Pg28khwAuBSeocC2PdVbPoUquj5Gm4GfYvd2pt8VS"),
+      })
+      .then((res) =>
+        console.log(
+          res.items.filter(
+            (x) =>
+              x.grouping[0].group_value ===
+              "6MGjmJZjMxJCy3Pxqt7C3CGvGWmYXfspmzuZf2cGWMXz",
+          ),
+        ),
+      );
+  }, [dasApiRpc, umi]);
+
   return (
     <div className="flex flex-col justify-center gap-4 overflow-hidden overflow-x-auto rounded-lg border bg-white p-4 shadow">
       <h3 className="text-center font-semibold">Menu</h3>
+
+      <div className="flex flex-col justify-center gap-4">
+        <div className="text-center">
+          <button
+            type="button"
+            className="btn btn-md btn-black"
+            onClick={handleStartNewGame}
+          >
+            Start new game
+          </button>
+        </div>
+
+        <div className="flex flex-row flex-wrap gap-4">{}</div>
+      </div>
     </div>
   );
 }

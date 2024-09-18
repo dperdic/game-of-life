2209c0ae58c860513bffa4c8162e00ca64ee30c2
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-18 21:23:33 +0200

    vars

--- game-of-life/next-env-addons.d.ts
+++ game-of-life/next-env-addons.d.ts
@@ -3,6 +3,8 @@ declare global {
     interface ProcessEnv {
       NEXT_PUBLIC_RPC_URL: string;
       NEXT_PUBLIC_SOL_CLUSTER: string;
+      NEXT_PUBLIC_MERKLE_TREE: string;
+      NEXT_PUBLIC_COLLECTION_NFT: string;
     }
   }
 }

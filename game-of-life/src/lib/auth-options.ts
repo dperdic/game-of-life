9d8d2c591a77290fafab9a4bd3fb9f479cb96a25
Dujo Perdic <dujo.perdic1@gmail.com>
2024-09-20 13:12:14 +0200

    refactored auth, added support for server actions

---
+++ game-of-life/src/lib/auth-options.ts
@@ -0,0 +1,73 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { Provider } from "next-auth/providers/index";
+import { getCsrfToken } from "next-auth/react";
+import { SigninMessage } from "@/utils/signature";
+import { AuthOptions } from "next-auth";
+
+const providers: Provider[] = [
+  CredentialsProvider({
+    name: "Solana",
+
+    credentials: {
+      signature: {
+        label: "Signature",
+        type: "text",
+      },
+      message: {
+        label: "Message",
+        type: "text",
+      },
+    },
+
+    async authorize(credentials, req) {
+      try {
+        const signinMessage = new SigninMessage(
+          JSON.parse(credentials?.message || "{}"),
+        );
+
+        const nextAuthUrl = new URL(process.env.AUTH_URL);
+
+        if (signinMessage.domain !== nextAuthUrl.host) {
+          return null;
+        }
+
+        const csrfToken = await getCsrfToken({ req: { ...req, body: null } });
+
+        if (signinMessage.nonce !== csrfToken) {
+          return null;
+        }
+
+        const validationResult = await signinMessage.validate(
+          credentials?.signature || "",
+        );
+
+        if (!validationResult)
+          throw new Error("Could not validate the signed message");
+
+        return {
+          id: signinMessage.address,
+        };
+      } catch (e) {
+        return null;
+      }
+    },
+    type: "credentials",
+  }),
+];
+
+export const authOptions: AuthOptions = {
+  providers,
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    session({ session, token }) {
+      if (session.user) {
+        session.user.name = token.sub;
+      }
+
+      return session;
+    },
+  },
+};
